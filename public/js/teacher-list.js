8d487226a3dc522d5e1b5bb996ae82b2a185ade0
emilyzhao21 <emily_zdy@163.com>
2017-09-26 14:29:32 +0800

    完成讲师启用和注销功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -1,11 +1,43 @@
-define(['jquery','template'],function($,template){
+define(['jquery', 'template'], function ($, template) {
   $.ajax({
-    type:'get',
-    url:'/api/teacher',
-    dataType:'json',
-    success:function(data){
-      var  html = template('teacherListTpl',data);
+    type: 'get',
+    url: '/api/teacher',
+    dataType: 'json',
+    success: function (data) {
+      var html = template('teacherListTpl', data);
       $('#tbodyMsg').html(html);
+      //讲师启用和注销
+      $('.eod').on('click', function () {
+        //备份当前this的值 默认是DOM对象
+        var that = this;
+        //获取所需参数 tc_id tc_status，使用自定义属性
+        //closet(选择器),获得匹配选择器的第一个祖先元素，从当前元素开始沿 DOM 树向上
+        var td = $(this).closest('td');
+        var tcId = td.attr('data-tc-id');
+        var tcStatus = td.attr('data-tc-status');
+        //发送请求
+        $.ajax({
+          type: 'post',
+          url: '/api/teacher/handle',
+          data: {
+            tc_id: tcId,
+            tc_status: tcStatus
+          },
+          dataType: 'json',
+          success: function (data) {
+            if (data.code == 200) {
+              //将获取的tc_status设置为数据中的值
+              td.attr('data-tc-status', data.result.tc_status);
+              //判断值，显示按钮状态
+              if (data.result.tc_status == 0) {
+                $(that).text('启用');
+              } else {
+                $(that).text('注销');
+              }
+            }
+          }
+        });
+      });
     }
   });
 });
