7f8dc076b7537cd85083391fe28c53be538afce9
emilyzhao21 <emily_zdy@163.com>
2017-10-03 19:53:44 +0800

    完成讲师编辑和添加的提交功能

--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -3,9 +3,11 @@ define(['jquery', 'template', 'util'], function ($, template, util) {
   if (tcId) {
     //编辑功能
     $.ajax({
-      type: 'get',
+      type: 'post',
       url: '/api/teacher/edit',
-      data:{tc_id:tcId},
+      data: {
+        tc_id: tcId
+      },
       dataType: 'json',
       success: function (data) {
         if (data.code == 200) {
@@ -13,13 +15,37 @@ define(['jquery', 'template', 'util'], function ($, template, util) {
           data.result.operate = '编辑讲师';
           var html = template('teacherTpl', data.result);
           $('#teacherInfo').html(html);
-
+          //修改信息，需要提交tc_id 。但表单中没有此项，所以添加个隐藏标签 input:hidden
+          //表单元素都需要添加name和value
+          submitForm('/api/teacher/update');
         }
       }
     });
   } else {
     //添加功能
-    var html = template('teacherTpl',{operate:'添加讲师'});
+    var html = template('teacherTpl', {
+      operate: '添加讲师'
+    });
     $('#teacherInfo').html(html);
+    submitForm('/api/teacher/add');    
   }
+
+  //提交表单功能，编辑和添加的请求地址不同，所以封装个方法
+  function submitForm(url) {
+    //点击提交按钮时，发出请求
+    $('#teacherBtn').click(function () {
+      $.ajax({
+        type: 'post',
+        url: url,
+        data: $('#teacherForm').serialize(),
+        dataType: 'json',
+        success: function (data) {
+          if (data.code == 200) {
+            location.href = '/teacher/list';
+          }
+        }
+      });
+      return false;//阻止submit的默认提交行为。get
+    })
+  };
 });
