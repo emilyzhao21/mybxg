50d9bd39d9f7ea48a7c5f20a4f2dcc95bb26938e
emilyzhao21 <emily_zdy@163.com>
2017-10-03 20:52:33 +0800

    添加日期插件

--- public/js/config.js
+++ public/js/config.js
@@ -6,6 +6,8 @@ require.config({
     cookie: 'jquery-cookie/jquery.cookie',
     template: 'artTemplate/template-web',
     bootstrap: 'bootstrap/js/bootstrap',
+    datepicker:'bootstrap-datepicker/js/bootstrap-datepicker',
+    language:'bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.min',
     util:'../js/util',
     common: '../js/common', //是common.js的文件模块化define
     login: '../js/login',
@@ -16,6 +18,10 @@ require.config({
     //bootstrap不是标准模块，依赖jQuery，使用shim属性将非标准模块转化为标准模块
     bootstrap: {
       deps: ['jquery'] //deps 固定语法
+    },
+    //language是非标准化模块，依赖于jQuery和datepicker
+    language:{
+      deps:['jquery','datepicker']
     }
   }
 });
